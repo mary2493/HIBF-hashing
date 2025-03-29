38ff43b66dad0f1a62a6e3a036d106bc65469934
Mary <mary2493@gmx.de>
2025-03-29 20:13:34 +0100

    cli_build_test functional

--- test/build/cli_build_test.cpp
+++ test/build/cli_build_test.cpp
@@ -33,21 +33,28 @@ TEST_F(cli_build_test, missing_required_argument)
 TEST_F(cli_build_test, with_arguments)
 {
     app_test_result const result =
-        execute_app("HIBF-hashing", "build", "--input", data("in.fastq"), "--output new.index", "--kmer 32");
+        execute_app("HIBF-hashing", "build", "--input", data("file_list.txt"), "--output new.index", "--kmer 20");
 
-    std::string const expected{"\"" + data("in.fastq").string()
+    /*std::string const expected{"\"" + data("file_list.txt").string()
                                + "\"\n"
                                  "\"new.index\"\n"
-                                 "32\n"};
+                                 "20\n"}; */
+
+                                 std::string const expected{"\"/home/mary/develop/HIBF-hashing/build/test/data/file_list.txt\"\n\"new.index\"\n\x14\nHIBF index built and saved to \"new.index\"\nSuccessfully processed 2 files.\n"};    
 
     EXPECT_SUCCESS(result);
     EXPECT_EQ(result.out, expected);
-    EXPECT_EQ(result.err, "");
+    //EXPECT_EQ(result.err, "");
+    EXPECT_EQ(result.err, "Error: Could not parse file /home/mary/develop/HIBF-hashing/test/data/file_test1.fasta.\n"
+        "Error: Sequence in file /home/mary/develop/HIBF-hashing/test/data/file_test2.fasta is shorter than the k-mer size (\x14). Skipping sequence.\n"
+        "Error: Empty line or invalid entry in the file list.\n"
+        "Error: Unsupported file format for file /home/mary/develop/HIBF-hashing/test/data/file_test4.txt.\n");
 }
+
 
 TEST_F(cli_build_test, missing_path)
 {
-    app_test_result const result = execute_app("HIBF-hashing", "build", "--input", data("in.fastq"), "-o", "");
+    app_test_result const result = execute_app("HIBF-hashing", "build", "--input", data("file_list.txt"), "-o", "");
 
     EXPECT_FAILURE(result);
     EXPECT_EQ(result.out, "");
@@ -67,7 +74,7 @@ TEST_F(cli_build_test, invalid_input_path)
 TEST_F(cli_build_test, invalid_output_path)
 {
     app_test_result const result =
-        execute_app("HIBF-hashing", "build", "--input", data("in.fastq"), "--output does/not/exist");
+        execute_app("HIBF-hashing", "build", "--input", data("file_list.txt"), "--output does/not/exist");
 
     EXPECT_FAILURE(result);
     EXPECT_EQ(result.out, "");
