0d01abe59f8b41979ade75d02d1e784d34807be3
Mary <mary2493@gmx.de>
2025-04-10 13:37:00 +0200

    api_build_test added minimiser

--- test/build/api_build_test.cpp
+++ test/build/api_build_test.cpp
@@ -11,12 +11,13 @@
 struct api_build_test : public app_test
 {};
 
-TEST_F(api_build_test, default_config)
+TEST_F(api_build_test, default_config_kmer)
 {
     configuration config{};
     config.file_list_path = data("file_list_for_tests.txt");
-    config.index_output = "new.index";
+    config.index_output = "new_kmer.index";
     config.kmer_size = 20;
+    config.hash = hash_type::kmer;
     testing::internal::CaptureStdout();
     testing::internal::CaptureStderr();
 
@@ -25,7 +26,29 @@ TEST_F(api_build_test, default_config)
     std::string const std_cout = testing::internal::GetCapturedStdout();
     std::string const std_cerr = testing::internal::GetCapturedStderr();
 
-    std::string const expected_cout{"HIBF index built and saved to \"new.index\"\n"
+    std::string const expected_cout{"HIBF index built and saved to \"new_kmer.index\"\n"
+                                    "Successfully processed 3 files.\n"};
+
+    EXPECT_EQ(expected_cout, std_cout);
+}
+TEST_F(api_build_test, default_config_minimiser)
+{
+    configuration config{};
+    config.file_list_path = data("file_list_for_tests.txt");
+    config.index_output = "new_minimiser.index";
+    config.kmer_size = 20;
+    config.window_size = 22;
+    config.hash = hash_type::minimiser;
+
+    testing::internal::CaptureStdout();
+    testing::internal::CaptureStderr();
+
+    build(config);
+
+    std::string const std_cout = testing::internal::GetCapturedStdout();
+    std::string const std_cerr = testing::internal::GetCapturedStderr();
+
+    std::string const expected_cout{"HIBF index built and saved to \"new_minimiser.index\"\n"
                                     "Successfully processed 3 files.\n"};
 
     EXPECT_EQ(expected_cout, std_cout);
